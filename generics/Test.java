1983d126610c21573fec321e8055185733a3863d
xiaoxiaobai2 <778998010@qq.com>
2019-08-26 18:13:11 +0800

    2019.08.26

--- generics/Test.java
+++ generics/Test.java
@@ -1,11 +1,16 @@
 package generics;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
         Javabean<String,Integer> bean = new Javabean<>();//在此处确定将要存储的参数类型
         bean.setName("zhangh");
+        Map map=new HashMap();
+        map.put(1,2);
+        System.out.println(map.get(2));
         System.out.println(bean.getName());
     }
 }
